13b54507f74682de06de10b1dbf6121bebd0c9f8
Mats100 <muneeb75@icloud.com>
2022-09-23 15:57:20 +0500

    added more information

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ async def get_data():
     url = f'http://api.weatherstack.com/current?access_key=35d3d2527e9de163d12b7b328245f4e6&query={city_name}'
     response = requests.get(url)
     api_response = response.json()
+    print(f'Latitude = {(api_response["location"]["lat"])}')
+    print(f'Longitude = {(api_response["location"]["lon"])}')
     print(f'There is {str(api_response["current"]["temperature"])} degree outside and it feels'
           f'like {str(api_response["current"]["feelslike"])} degree and '
           f'{str(api_response["current"]["precip"])} % chance of rain and humidity'
-          f' is {str(api_response["current"]["humidity"])} %')
+          f' is {str(api_response["current"]["humidity"])} %'
+          f' and visibility is {str(api_response["current"]["visibility"])}')
 
 asyncio.run(get_data())
